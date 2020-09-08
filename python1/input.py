9d1008c4267ea3031450e280b0f2f2d9559be8b1
Karolina Grabowska <53982166+grabowskakarolina@users.noreply.github.com>
2020-09-08 21:28:59 +0200

    Initial commit

---
+++ python1/input.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#%%
+imie = input('podaj swoje imie:')
+print('czesc', imie)
+#%%
+JEZYK = input('JAKIEGO JEZYKA CHCESZ SIE NAUCZYC?')
+print('podano jezyk {}'.format(JEZYK))
+
+#%%
+name = input('podaj imie: ')
+age = input('podaj wiek: ')
+
+print('czesc {}! Masz {} lat'.format(name, age))
+
+#%%
+
